568f4511f03efd68c0628f14e951a4dc60fc0e2d
Enzo Sampaio <enzosenzos9@gmail.com>
2026-09-16 16:44:03 -0300

    criar rota /resultado/{tarefa_id}
    .

--- app/api_rest.py
+++ app/api_rest.py
@@ -61,11 +61,12 @@ def predict(entrada: Entrada):
 
 
 
-# ------------------------------------------------------------------
-# TAREFA 2 - consulta do resultado
-# ------------------------------------------------------------------
-# @app.get("/resultado/{tarefa_id}")
-# def resultado(tarefa_id: str):
-#     """Deve devolver o resultado; 404 se o id nao existir."""
-#     # DICA: use app.fila.buscar_resultado(tarefa_id)
-#     raise NotImplementedError("implemente a consulta de resultado")
+@app.get("/resultado/{tarefa_id}")
+def resultado(tarefa_id: str):
+    """Deve devolver o resultado; 404 se o id nao existir."""
+    res = app.fila.buscar_resultado(tarefa_id)
+    
+    if res is None:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    
+    return res
